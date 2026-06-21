bc6b9a7301bbc38f802a5f94d05116bbfcb5573c
Swikar Paudel <141716870+SwikarPaudel@users.noreply.github.com>
2026-06-21 19:34:39 +0545

    User component is added

--- reactRouter06/src/main.jsx
+++ reactRouter06/src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
-
+import User from './components/User/User.jsx'
 const router = createBrowserRouter([
   {
     path: '/',
@@ -24,10 +24,25 @@ const router = createBrowserRouter([
       {
         path: "contact",
         element: <Contact/>
-      }
+      },
+      {
+        path: "user/:userid",
+        element: <User/>
+      },
     ]
   }
 ])
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path='/' element={<Layout/>}>
+//       <Route path='' element={<Home/>}/>
+//       <Route path='about' element={<About/>}/>
+//       <Route path='contact' element={<Contact/>}/>
+
+//     </Route>
+//   )
+// )
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
