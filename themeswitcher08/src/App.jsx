53c0a37929e01777e404eceb95bef659a7fdea7d
Swikar Paudel <141716870+SwikarPaudel@users.noreply.github.com>
2026-06-22 12:41:40 +0545

    theme.js is added

--- themeswitcher08/src/App.jsx
+++ themeswitcher08/src/App.jsx
@@ -1,16 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>hi</h1>
-    </>
+    
+<div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        {/* ThemeButton */}
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       {/* Card */}
+                    </div>
+                </div>
+            </div>
+
   )
 }
 
