8a1490c39c9fcdd15848eee5de84fd48cb8a7197
Swikar Paudel <141716870+SwikarPaudel@users.noreply.github.com>
2026-06-22 13:36:31 +0545

    custom dark theme is added in tailwind

--- themeswitcher08/src/App.jsx
+++ themeswitcher08/src/App.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import './App.css'
 import { ThemeProvider } from './contexts/Theme'
+import ThemeBtm from './components/ThemeBtm'
+import Card from './components/Card'
 
 function App() {
   const [themeMode, setThemeMode] = useState("light")
@@ -17,19 +19,19 @@ function App() {
   //actual change in theme
 
   useEffect(() => {
-    document.querySelector('html').classList.remove("light"," dark")
+    document.querySelector('html').classList.remove("light","dark")
     document.querySelector('html').classList.add(themeMode)
   }, [themeMode])
 
   return (
-
     <ThemeProvider value = {{themeMode, lightTheme, darkTheme}}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
             {/* ThemeButton */}
+            <ThemeBtm />
           </div>
-
+            <Card />
           <div className="w-full max-w-sm mx-auto">
             {/* Card */}
           </div>
