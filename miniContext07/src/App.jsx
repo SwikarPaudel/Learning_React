5663e370d11754270ba1ee67f5f5a53162280ac7
Swikar Paudel <141716870+SwikarPaudel@users.noreply.github.com>
2026-06-22 12:04:35 +0545

    Login and Profile component is added and data is fetch using context api in userContextProvider,jsx

--- miniContext07/src/App.jsx
+++ miniContext07/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Profile from './components/Profile'
+import Login from './components/Login'
 
 function App() {
   
 
   return (
-    <>
+    <UserContextProvider>
      <h1>react</h1>
-    </>
+     <Login />
+     <Profile />
+    </UserContextProvider>
   )
 }
 
