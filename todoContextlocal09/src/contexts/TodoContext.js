fec306cfdb37c02cdbaf137d4d31c5ce06eb622a
Swikar Paudel <141716870+SwikarPaudel@users.noreply.github.com>
2026-06-24 12:44:05 +0545

    Feature: TodoForm and TodoItem  components are added

--- todoContextlocal09/src/contexts/TodoContext.js
+++ todoContextlocal09/src/contexts/TodoContext.js
@@ -1,19 +1,22 @@
-import { createContext, useContext } from "react";
+import {createContext, useContext} from "react"
 
 export const TodoContext = createContext({
-    todos:[
-       { id:1,
-        todo: "Todo msg",
-        completed: false,}
+    todos: [
+        {
+            id: 1,
+            todo: " Todo msg",
+            completed: false,
+        }
     ],
     addTodo: (todo) => {},
-    updatedTodo: (id, todo) => {},
-    deletTodo: (id) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
     toggleComplete: (id) => {}
 })
+
 
 export const useTodo = () => {
     return useContext(TodoContext)
 }
 
-export const TodoContext = TodoContext.Provider
+export const TodoProvider = TodoContext.Provider
