aa7d9ae18a0261c7206def0345570828245fcc5a
Swikar Paudel <141716870+SwikarPaudel@users.noreply.github.com>
2026-07-15 13:26:20 +0545

    AddPost page

--- BlogWebsite/src/components/index.js
+++ BlogWebsite/src/components/index.js
@@ -1,3 +1,4 @@
+import Select from "./Select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
@@ -6,8 +7,24 @@ import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
 import Signup from "./Signup";
 import Login from "./Login";
-
-export {Header, Footer
-, Container, Logo, LogoutBtn,
-RTE, Signup, Login
-};
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout, 
+    Input,
+    Select,
+}
