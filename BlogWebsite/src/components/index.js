6b3cae8aa940e1d981646a360ec58b6248d1e83e
Swikar Paudel <141716870+SwikarPaudel@users.noreply.github.com>
2026-07-15 13:21:19 +0545

    Signup Page

--- BlogWebsite/src/components/index.js
+++ BlogWebsite/src/components/index.js
@@ -3,7 +3,10 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import signup from "./Signup";
 
 export {Header, Footer
-, Container, Logo, LogoutBtn
+, Container, Logo, LogoutBtn,
+RTE, Signup
 };
